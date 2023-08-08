e29a5a1b045fbfb514376de9e3fd530d6ea85386
Yakir Gagnon <12.yakir@gmail.com>
2023-08-08 22:38:29 +0200

    wip

--- new/main.jl
+++ new/main.jl
@@ -24,7 +24,9 @@ function get_exit_point(nsteps, brw, crw, w)
     return xy
 end
 
-mean_resultant_length(nrepetitions, nsteps, brw, crw, w) = norm(Folds.mapreduce(_ -> get_exit_point(nsteps, brw, crw, w), +, 1:nrepetitions)/nrepetitions) 
+mean_resultant_length(nrepetitions, nsteps, brw, crw, w) = norm(Folds.mapreduce(_ -> get_exit_point(nsteps, brw, crw, w), +, 1:nrepetitions)/nrepetitions/nsteps) 
+
+fwhm(κ) = rad2deg(2acos(log(cosh(κ))/κ))
 
 # figure 4:
 
@@ -34,7 +36,7 @@ crw_κ = 4 # equivalent to an "angular deviation" of 30°
 n = 100
 
 w = range(0, 1, n)
-brw_κ = range(0.01, 10, n)
+brw_κ = range(0.01, 400, n)
 r = Folds.map(κw -> mean_resultant_length(nrepetitions, nsteps, VonMises(κw.κ), VonMises(crw_κ), κw.w), ((; κ, w) for w in w, κ in brw_κ))
 
 fig = Figure()
@@ -43,3 +45,10 @@ heatmap!(ax, w, brw_κ, r)
 Colorbar(fig[1,2], label="Mean resultant length", limits=(0,1))
 
 save("figure 4.png", fig)
+
+fig = Figure()
+ax = Axis(fig[1,1], xlabel="Weight", ylabel="Compass FWHM", xticks = (0:0.5:1, ["no compass", "0.5", "no motor"]), ytickformat = "{:d}°", limits = (nothing, (nothing, 120)))
+h = contour!(ax, w, fwhm.(brw_κ), r, levels=[0.75, 0.8, 0.9, 0.95, 0.99, 1], labels=true)
+
+
+save("contour.png", fig)
